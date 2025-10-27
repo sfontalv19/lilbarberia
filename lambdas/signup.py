7400c783ab4c8f67f0146ad32e2f4bc34c8dec40
sergio Fontalvo <serme1009@gmail.com>
2025-10-27 15:49:42 -0500

    fix lambda signup

--- lambdas/signup.py
+++ lambdas/signup.py
@@ -5,12 +5,14 @@ from botocore.exceptions import ClientError
 default_headers = {
     "Access-Control-Allow-Origin": "*",
     "Access-Control-Allow-Methods": "OPTIONS,POST,GET",
-    "Access-Control-Allow-Headers": "Content-Type",
-    "Content-Type": "application/json",
+    "Access-Control-Allow-Headers": "Content-Type,Authorization,X-Amz-Date,X-Api-Key,X-Amz-Security-Token",
 }
 
 def resp(status, body):
-    return {"statusCode": status, "headers": default_headers, "body": json.dumps(body)}
+    return {
+        "statusCode": status,
+        "headers": default_headers,
+        "body": json.dumps(body)}
 
 def handler(event, context):
     try:
